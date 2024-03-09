9c82fc6042b47249b66d5191029f4fe9376c407c
EC2 Default User <ec2-user@ip-172-31-65-111.ec2.internal>
2024-03-09 00:49:08 +0000

    updated terraform files

--- terraform_code/dev/output.tf
+++ terraform_code/dev/output.tf
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 # Add output variables
 output "eip" {
   value = aws_eip.static_eip.public_ip
-=======
-# Add output variables
-output "eip" {
-  value = aws_eip.static_eip.public_ip
->>>>>>> 9e745dc (Added files from Assignment 1)
 }
--- terraform_code/dev/variables.tf
+++ terraform_code/dev/variables.tf
@@ -1,9 +1,8 @@
-<<<<<<< HEAD
 # Instance type
 variable "instance_type" {
   default = {
-    "prod"    = "t3.medium"
-    "test"    = "t3.micro"
+    "prod"    = "t3.large"
+    "test"    = "t3.large"
     "staging" = "t2.micro"
     "dev"     = "t2.micro"
   }
@@ -16,23 +15,4 @@ variable "env" {
   default     = "dev"
   type        = string
   description = "Deployment Environment"
-=======
-# Instance type
-variable "instance_type" {
-  default = {
-    "prod"    = "t3.medium"
-    "test"    = "t3.micro"
-    "staging" = "t2.micro"
-    "dev"     = "t2.micro"
-  }
-  description = "Type of the instance"
-  type        = map(string)
-}
-
-# Variable to signal the current environment 
-variable "env" {
-  default     = "dev"
-  type        = string
-  description = "Deployment Environment"
->>>>>>> 9e745dc (Added files from Assignment 1)
 }
